af3d15a90cb7497e6c6589099d23d13bfeda3dd7
pritam <pritammaity123456@gmail.com>
2019-05-25 17:16:37 +0530

    login and signup start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { P404Component } from './common/p404/p404.component';
+import { LoginComponent } from './common/login/login.component';
+import { RegisterComponent } from './common/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    P404Component
+    P404Component,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
